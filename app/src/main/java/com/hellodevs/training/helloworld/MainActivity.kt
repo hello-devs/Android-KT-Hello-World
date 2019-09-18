3b571c378c8256ad681956e6d0b11a861dcd54c1
hello-devs-pc-home <cirer972@gmail.com>
2019-09-18 23:19:37 +0200

    Learn: controle de flux if else when + attribution ternaire

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -9,28 +9,64 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var age: Int = 10
-        var name: String = "Bob"
+        var age: Int = 5
+        var name: String = "Jul"
+        var height: Float = 1.60F
 
-        println("string template simple: $name a $age ans")
-        println("string template complexe: ${name.toUpperCase()} a ${age + 5} ans")
-        println("Concatenation: \n"
-                + name + " a " + age + "ans")
+        //Controle de flux if else
+        if(age < 5){
+            println("${name} est trop jeune pour jouer au basket!")
+        } else {
+            println("${name} peut jouer au basket! \n de plus il mesure $height")
+        }
 
-        println("""Raw string: 
-Nom: $name
-Age: $age
-        """)
+        if("Bob" == name)
+        {
+            println("${name} est un garçon")
+        } else if ("nathalie" == name){
+            println("${name} est une fille")
+        } else {
+            println("On ne connait pas le sexe de ${name}")
+        }
 
-        println("""Raw string trim:
-            |Nom: $name
-            |Age: $age
-        """.trimMargin())
+        //Operateur ternaire
+        println("-----")
 
-        println("""Raw string trim2:
-            >Nom: $name
-            >Age: $age
-        """.trimMargin(">"))
+        val type = if (age < 18) "mineur" else "majeur"
+        println(type)
+
+        //Controle de flux when
+        println("-----")
+        when (age){
+            5 -> println("${name} a ${age} ans")
+            10 -> println("${name} a ${age} ans")
+            18 -> println("${name} est ${age} ans")
+            else -> println("On ne connait pas l'age de ${name}")
+        }
+
+        println("--")
+
+        when{
+            "Bob" == name ->println("${name} est un garçon")
+            "nathalie" == name -> println("${name} est une fille")
+            else  -> println("On ne connait pas le sexe de ${name}")
+        }
+
+        println("--")
+        //Intervale
+        when (age){
+            in 0..17 -> println("${name} est mineur")
+            !in 0..17-> println("${name} ne peut pas jouer avec les enfants")
+            else -> println("Condition non gérée")
+        }
+
+        //attribution
+        val canPlayWithChild = when (age){
+            in 0..17 -> true
+            else -> false
+        }
+        println(canPlayWithChild)
+
 
     }
 }
