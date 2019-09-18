b47c423eb796d8c80d3b95eefc8b7eca5e54fc9d
hello-devs-pc-home <cirer972@gmail.com>
2019-09-18 18:29:54 +0200

    Learn: definition de variable

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -8,5 +8,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var age: Int = 5
+        var name: String = "Bob"
+        var height: Float = 1.60F
+
+        println("${name} a ${age} ans")
+
+        age = age + 2
+
+        println("il a maintenant ${age} ans")
+
+        age *= 2
+
+        println("puis ${age} ans")
+        println("puis ${height} ans")
+
     }
 }
