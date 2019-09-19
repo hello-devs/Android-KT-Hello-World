38bcfbd9264367b06624ec507368352b0837bce4
hello-devs <cirer972@gmail.com>
2019-09-19 11:15:10 +0200

    Learn: Mutable Immutable Nullable

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -9,63 +9,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var age: Int = 5
-        var name: String = "Jul"
-        var height: Float = 1.60F
 
-        //Controle de flux if else
-        if(age < 5){
-            println("${name} est trop jeune pour jouer au basket!")
-        } else {
-            println("${name} peut jouer au basket! \n de plus il mesure $height")
-        }
+        //Mutable / Immutable / Nullable
 
-        if("Bob" == name)
-        {
-            println("${name} est un garçon")
-        } else if ("nathalie" == name){
-            println("${name} est une fille")
-        } else {
-            println("On ne connait pas le sexe de ${name}")
-        }
+        //Mutable: var
+        var age = 5
 
-        //Operateur ternaire
-        println("-----")
+        //Immutable: val
+        val ageMinimum = 18
 
-        val type = if (age < 18) "mineur" else "majeur"
-        println(type)
+        //Nullable
+        var name: String? = "Bob"
+        name = null
 
-        //Controle de flux when
-        println("-----")
-        when (age){
-            5 -> println("${name} a ${age} ans")
-            10 -> println("${name} a ${age} ans")
-            18 -> println("${name} est ${age} ans")
-            else -> println("On ne connait pas l'age de ${name}")
-        }
-
-        println("--")
-
-        when{
-            "Bob" == name ->println("${name} est un garçon")
-            "nathalie" == name -> println("${name} est une fille")
-            else  -> println("On ne connait pas le sexe de ${name}")
-        }
-
-        println("--")
-        //Intervale
-        when (age){
-            in 0..17 -> println("${name} est mineur")
-            !in 0..17-> println("${name} ne peut pas jouer avec les enfants")
-            else -> println("Condition non gérée")
-        }
-
-        //attribution
-        val canPlayWithChild = when (age){
-            in 0..17 -> true
-            else -> false
-        }
-        println(canPlayWithChild)
+        if (name != null) {println(name.length)} //TEST
+        println(name?.length)  //Safe call Operator
+        //println(name!!.length)  //assert non-null Operator: accede meme si null et plante si null
 
 
     }
