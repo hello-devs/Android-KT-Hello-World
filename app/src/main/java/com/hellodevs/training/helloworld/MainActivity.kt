0b3b05e4885346906197287b4e8a8bcb5e19a5fb
hello-devs-pc-home <cirer972@gmail.com>
2019-09-20 22:29:38 +0200

    Learn: Boucle For

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -14,18 +14,64 @@ class MainActivity : AppCompatActivity() {
 
         //Syntaxe constructeur
         val array :Array<Int> =  Array<Int>(3) { 0 }
-        println(Arrays.toString(array))
 
         val names = Array<String>(3) {""}
         names.set(0, "Bob")
         names[1] = "John"
-        println(names.get(0))
-        println(names[1])
 
         //Factoy Function
-        val ages= arrayOf(4, 7, 55)
-        println("le troisieme élément est ${ages[2]}")
+        val ages= arrayOf(4, 7, 15, 35, 44, 55)
 
+        //Boucle For
+
+        //For avec range
+        for (i in 1..5){
+            println(i)
+        }
+
+        println("--")
+
+        //For avec range et step
+        for (i in 1..5 step 2){
+            println(i)
+        }
+
+        println("--")
+
+        //For avec range (downto)
+        for (i in 5 downTo 1){
+            println(i)
+        }
+
+        println("--")
+
+        //For avec range (downto)
+        for (i in 5 downTo 1 step 2){
+            println(i)
+        }
+
+        println("--")
+
+        //foreach
+        for (age in ages){
+            println(age)
+        }
+
+        println("--")
+
+        //foreach avec index
+        for ((index, age) in ages.withIndex()){
+            println("$age est a l'index $index")
+        }
+
+        //continue
+        for (age in ages){
+            if(age == 44) {
+                println("On affiche ceci puis on passe au tour de boucle suivant")
+                continue
+            }
+            println(age)
+        }
 
     }
 }
