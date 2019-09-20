fa44618229ac24578e7722239694fc4d2870c9f7
hello-devs <cirer972@gmail.com>
2019-09-20 11:30:54 +0200

    Learn: function

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -3,29 +3,48 @@ package com.hellodevs.training.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//Déclaration de fonction
+fun isOldEnough(age: Int): Boolean {
+    return  age >= 5
+}
+
+//Syntaxe courte fonction 1 ligne:
+fun isOldEnough2(age: Int): Boolean = age >= 5
+
+fun describePeople(age: Int, name: String, height: Float){
+    val canPlaySentence = when (isOldEnough2(age)){
+        true -> "peut jouer basketball"
+        false -> "ne peut pas jouer basketball"
+    }
+    println("$name a $age ans et mesure ${height}m et $canPlaySentence")
+
+}
+
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        //Mutable / Immutable / Nullable
-
-        //Mutable: var
         var age = 5
+        var name = "Jul"
+        var height = 1.60F
 
-        //Immutable: val
-        val ageMinimum = 18
+        var canPlayBasket = isOldEnough(4)
+        var canPlayBasket2 = isOldEnough2(10)
 
-        //Nullable
-        var name: String? = "Bob"
-        name = null
+        println(canPlayBasket)
+        println(canPlayBasket2)
 
-        if (name != null) {println(name.length)} //TEST
-        println(name?.length)  //Safe call Operator
-        //println(name!!.length)  //assert non-null Operator: accede meme si null et plante si null
+        describePeople(age, name, height)
 
+        age = 4
+        name = "Martin"
+        height = 1.10F
 
+        describePeople(age, name, height)
     }
 }
