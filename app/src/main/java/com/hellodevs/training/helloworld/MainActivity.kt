ccfeff7df6ee6af4f062d04fc18aaf1e745723c2
hello-devs-pc-home <cirer972@gmail.com>
2019-09-23 17:13:24 +0200

    Learn: POO Class constructor(s)

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -3,22 +3,27 @@ package com.hellodevs.training.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//Intro POO déclaration de class
+//POO constructeur de class
 
-class Car(val wheelsCount: Int){
-    fun honk(){
-        println("buzz")
-    }
+class User(
+    val name: String,
+    val age: Int,
+    val height: Float
+) {
 
-    fun honkForWheels(){
-        println("honkForWells")
+    val canPlayBasketball = age >= 5 && height >= 1.50f
 
-        for(i in 1..wheelsCount){
-            honk()
+    init {
+        val basketString = when (canPlayBasketball) {
+            true -> "est dans l'équipe de basketball"
+            false -> "ne peut pas jouer au basketball"
         }
-    }
-}
+        println("$name a $age ans et $basketString")
 
+    }
+    constructor(name: String): this(name, 5, 1.2f)
+
+}
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,11 +32,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val car = Car(4)
-        println("la voiture a ${car.wheelsCount} roues")
-
-        car.honk()
-        car.honkForWheels()
+        val jerome = User("Jérome", 15, 1.85f)  //
+        val bobette = User("Bobette")                       //Fait appel au construceur sec pour les valeur manquante
     }
 }
