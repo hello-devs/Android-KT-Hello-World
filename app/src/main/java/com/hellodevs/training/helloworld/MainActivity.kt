de7fa0c141fe354f7b899f768927ad0417538995
hello-devs <cirer972@gmail.com>
2019-10-01 16:09:21 +0200

    Learn: ANDROID OS Dimension

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -6,28 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-//ANDROID OS: Parcelable (serialize/deserialize)
+//ANDROID OS: Dimension
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
-
-    val user: User = User("bob",15)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-
-        start_activity_green_btn.setOnClickListener {
-
-            val intent = Intent(this, UserDetailActivity::class.java)
-
-            intent.putExtra("user",user)
-
-            startActivity(intent)
-        }
     }
 }
 
