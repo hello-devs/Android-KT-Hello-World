016ccd667a23d79c2abe271a8df7f87a4edbefe8
hello-devs <cirer972@gmail.com>
2019-10-10 11:07:45 +0200

    Learn: ANDROID OS OptionManu

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -3,11 +3,11 @@ package com.hellodevs.training.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
-//ANDROID OS: Dialog (modal)
-
+//ANDROID OS: OptionMenu
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
@@ -16,27 +16,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
 
-        show_dialog_button.setOnClickListener {
+    /* Afficher le menu*/
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)     //inflater transform le fichier xml en code
+        return true //indique que l'activité contient un menu et qu'il faut l'afficher
+    }
 
-            val fragment = ConfirmDeleteDialogFragment()
-            fragment.listener = object: ConfirmDeleteDialogFragment.ConfirmDeleteListener {
-                override fun onDialogPositiveClick() {
-                    Log.i(TAG,"onDialogPositiveClick")
-
-                    val fileListFragment = FileListDialogFragment()
-                    fileListFragment.show(supportFragmentManager,"fileList")
-                }
-
-                override fun onDialogNegativeClick() {
-                    Log.i(TAG, "onDialogNegativeClick")
-                }
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_add -> {
+                Toast.makeText(this, "Ajouter", Toast.LENGTH_SHORT).show()
+                return true
             }
-            fragment.show(supportFragmentManager, "confirmDelete")
+            R.id.action_save -> {
+                Toast.makeText(this, "Sauvegarder", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_delete -> {
+                Toast.makeText(this, "Supprimer", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_help -> {
+                Toast.makeText(this, "Aide", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
         }
-
-
     }
 }
 
