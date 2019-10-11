65b9b0a42a51d247704a082d012636edf32c447b
hello-devs <cirer972@gmail.com>
2019-10-11 11:15:32 +0200

    Learn: ANDROID OS Theme

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -3,11 +3,9 @@ package com.hellodevs.training.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 
-//ANDROID OS: OptionMenu
+
+//ANDROID OS: Theme
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
@@ -18,33 +16,5 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    /* Afficher le menu*/
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)     //inflater transform le fichier xml en code
-        return true //indique que l'activité contient un menu et qu'il faut l'afficher
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.action_add -> {
-                Toast.makeText(this, "Ajouter", Toast.LENGTH_SHORT).show()
-                return true
-            }
-            R.id.action_save -> {
-                Toast.makeText(this, "Sauvegarder", Toast.LENGTH_SHORT).show()
-                return true
-            }
-            R.id.action_delete -> {
-                Toast.makeText(this, "Supprimer", Toast.LENGTH_SHORT).show()
-                return true
-            }
-            R.id.action_help -> {
-                Toast.makeText(this, "Aide", Toast.LENGTH_SHORT).show()
-                return true
-            }
-
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
 }
 
