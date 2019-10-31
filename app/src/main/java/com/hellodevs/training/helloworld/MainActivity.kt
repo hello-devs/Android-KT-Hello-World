b9eedd44d58c6cb8dba5cc10c660f31a002e2885
hello-devs <cirer972@gmail.com>
2019-10-31 11:55:59 +0100

    Learn : Kotlin+ : Exception Throw Try/Catch/Finally 1/2

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.hellodevs.training.helloworld
 
-//Kotlin+ : High Order Function Lambda 2/2
+//Kotlin+ : Exception Throw Try/Catch/Finally 1/2
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-fun arrayAction(array: Array<Int>, action: (Int)-> Unit){
-    for (a in array){
-        action(a)
-    }
+fun divide(numerator: Int?, denominator: Int?) : Int {
+    return numerator!! / denominator!!
+}
 
+////////////////////////////////////////////////////
+class DivideException(message: String, cause: Exception)
+    : Exception(message, cause)
+
+fun divideWithDivideException(numerator: Int?, denominator: Int?) : Int {
+    try {
+        return numerator!! / denominator!!
+    }catch (e: NullPointerException ){
+        throw DivideException("Opérande Null", e)
+    } catch (ae: ArithmeticException){
+        throw DivideException("Division par 0 interdite", ae)
+    }
 }
 
 class MainActivity : AppCompatActivity() {
@@ -22,36 +31,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val numbers = arrayOf(-99, 56, - 39, 11, 23, 78, 66, 0, -1, -30)
+        val numerator : Int? = 10
+        val denominator = 0
 
-        numbers.filter { number -> number > 0}
-        //Si un seul parametre équivaut à:
-        numbers.filter { it > 0 }
+        try {
+            println("Début de l'opération...")
+            divide(numerator, denominator)
+            println("Opération effectuer avec succès")
+        }catch (e: NullPointerException){
+            println("${e.message}, cause: ${e.cause}")
+        }catch (ae: ArithmeticException){
+            println("${ae.message}, cause: ${ae.cause}")
+        }finally {
+            println("Fin de l'opération")
+        }
 
-        arrayAction(numbers, {number -> println(number)})
-        //Si lambda est le dernier paramètre équivaut à:
-        arrayAction(numbers) { number -> println(number)}
-        //En fonction kotlin équivaut à:
-        numbers.forEach { println(it) }
-        
-        numbers.forEachIndexed { index, i -> println("index = $index, valeur = $i")  }
-        //On peut ignorer un paramètre avec l'underscore
-        numbers.forEachIndexed { _, i -> println("valeur = $i")  }
+        /////////////////////////////////////////////
 
-        //Dans ANDROID:
-        val button = Button(this)
+       try {
+           println("Opération 1:")
+           divideWithDivideException(10, 1)
+           //divideWithDivideException(10, 0)  //test erreur
+           println("Opération 2:")
+           divideWithDivideException(null, 10)
+           println("Opérations effectuer avec succès")
+       }catch (de: DivideException){
+           println("${de.message}, cause: ${de.cause}")
+       }finally {
+           println("Fin des opérations")
+       }
 
-        ///////////////////////////////////////////////////////////////////
-        button.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(view: View?) {
-                println("click")
-            }
-        })
-        //équivaut avec une lambda à:
-        button.setOnClickListener { view -> println("click") }
-        //On peut simplifier:
-        button.setOnClickListener { println("Click") }
-        ///////////////////////////////////////////////////////////////////////
+
+
+
+
+
 
     }
 }
