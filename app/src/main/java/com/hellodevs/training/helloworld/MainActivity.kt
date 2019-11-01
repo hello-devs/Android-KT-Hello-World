7f30adc854782458f93c1bb4b2fe1dd202c02f12
hello-devs <30301241+hello-devs@users.noreply.github.com>
2019-11-01 18:40:06 +0100

    Learn: Kotlin+: Elvis Operator

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -1,34 +1,26 @@
 package com.hellodevs.training.helloworld
 
-//Kotlin+ : Exception Preconditions require() check() 2/2
+//Kotlin+ : Elvis Operator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.IllegalArgumentException
 
-fun validateName(name: String){
-    require(name.isNotEmpty())  {"empty name!"}
-    for (character in name){
-        require(character.isLetter()) {"Invalid name: non letter character ($character)"}
-    }
-}
+data class User(var name: String?, var email: String?){
 
-fun sendGift(user: User){
-    require(user.email.isNotEmpty()) {"Email required"}
-    check(user.state == User.State.ACTIVE) {"Invalid user state: ${user.state}"}
-    println("sending gift to user: ${user.name} now!")
-}
-
-data class User(val name: String, val email: String){
-
-    enum class State {
-        NEW,
-        ACTIVE
+    fun updateName(name: String?){
+        this.name = name ?: "NoName"
     }
 
-    var state: State = State.NEW
+    fun updateEmail(email: String?){
+        this.email = email ?: throw IllegalArgumentException("Invalid Email")  //Permet aussi de jeter des exception
+    }
 
-    init {
-        validateName(name)
+    fun getInfoLength() {
+        val nameLength = name?.length ?: 0
+        val emailLength = email?.length ?: 0
+        val infoLength = nameLength + emailLength
+        println("Info length: $infoLength")
     }
 }
 
@@ -40,12 +32,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        validateName("Jul")
+        val name: String? = "Bob"
 
-//        val bobette = User("bob3773","bobette@mail.com")      //test require
-        val bobette = User("bobette","bobette@mail.com")        //test check
-        bobette.state = User.State.ACTIVE  //commenter pour tester l'exception
-        sendGift(bobette)
+        val size = if (name != null) name.length else 0
+        //équivaut à:
+        val sizeElvis = name?.length ?: 0
+
+        val bob = User("bob", null)
+        bob.updateName(null)
+        println(bob)
+
+        bob.updateName("bob")
+        bob.updateEmail("bob@mail.com")
+        println(bob)
+
+//        bob.updateEmail(null) //Throw Exception
+        bob.getInfoLength()
+
+
     }
 }
 
