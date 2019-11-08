765400e4fcfbd91b648fd4512f31d7bbdd96b245
hello-devs <cirer972@gmail.com>
2019-11-08 12:44:22 +0100

    Learn: Kotlin+ : Lazy init

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -1,48 +1,40 @@
 package com.hellodevs.training.helloworld
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-//Kotlin+ : Collection Map 3/3
+//Kotlin+ : Lazy init
+/*
+Variable immutable initialisée lors de sa premiere utilisation
+ */
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
 
+    val helloDevsLazy: TextView by lazy {
+        println("lazy init")
+        hello_devs
+    }
+
+    lateinit var helloDevsLate : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val languages = mutableMapOf(
-            "Kotlin" to "Un super langage",
-            "Java" to "Un langage qui a fait son temps"
-        )
-        languages.put("C++","Une des origines du Java")
-        languages["C#"] = "Une dérivé du C"
-        println(languages)
-        println("valeur de la clef \"Ktolin\": ${languages["Kotlin"]}" )
+        println("Lazy: ${helloDevsLazy.text}")  //Première appel init de la variable
 
-        if (!languages.containsKey("Python")){
-            println("Python n'est pas présent!")
-        }
+//        helloDevsLate = hello_devs
 
-        for (key in languages.keys){
-            println("Clef: $key")
-        }
-        
-        for (entry in languages.entries){
-            println("Clef: ${entry.key} => Valeur: ${entry.value} ")
-        }
+    }
 
-        for ((key, value) in languages){
-            println("Clef: $key ===> Valeur: $value ")
-        }
-
-        val nonCppLanguages = languages
-            .filterNot { it.key == "C++" } //équivaut à filter { it.key != "C++" }
-            .mapValues { it.value.toUpperCase() }
-
-        println("nonCppLanguages: $nonCppLanguages")
+    override fun onResume() {
+        super.onResume()
+        println("Lazy: ${helloDevsLazy.text}")  //Deuxième appel la variable a déjà été init
+//        println("Late: ${helloDevsLate.text}")  //Génère une exception si on oublie d'initialiser
     }
 }
 
