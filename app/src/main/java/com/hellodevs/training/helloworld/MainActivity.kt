40034109b07b57b7828b7618b02c1deef094dbb0
hello-devs <cirer972@gmail.com>
2019-11-12 12:38:40 +0100

    Learn : Kotlin Extension de fct : use

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hellodevs.training.helloworld
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
 
-//Kotlin Extension de fonction : run
+//Kotlin Extension de fonction : use
 /*
-Combinaison de let et with
+Ferme une classe qui implement l'interface Closeable
  */
 
 class MainActivity : AppCompatActivity() {
@@ -16,19 +21,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sb = StringBuilder()
-        sb.append("Hello! ")
-        sb.append("Kotlin")
-
-        val sentence = sb.toString()
-        println(sentence)
-
-        val shorterSentence = StringBuilder().run {
-            append("hello! ")
-            append("rapide run()")
-            toString()
+        val properties = Properties()
+        with(properties){
+            setProperty("name", "Bob")
+            setProperty("age", "10")
         }
-        println(shorterSentence)
+
+        val file = File("config.properties")
+
+        /////////////Kotlin Style
+        FileOutputStream(file).use { fileOutputStream ->
+            properties.store(fileOutputStream, null)
+        }
+
+        val loadProperties = properties.apply {
+            FileInputStream(file).use { load(it) }
+        }
+
+        /////////////Java Style
+        var fileOutputStream : FileOutputStream? = null
+        try {
+            fileOutputStream = FileOutputStream(file)
+            properties.store(fileOutputStream, null)
+        }catch (e: IOException){
+
+        }finally {
+            if (fileOutputStream != null){
+                try {
+                    fileOutputStream.close()
+                }catch (e: IOException){
+
+                }
+            }
+        }
+        //.....
+        /////////////////////////////////////////////////
+
     }
 }
 
