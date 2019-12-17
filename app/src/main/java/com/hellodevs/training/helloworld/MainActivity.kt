81927f92fe8747efc0e8542acd7a765880c2a540
hello-devs <cirer972@gmail.com>
2019-12-17 11:59:04 +0100

    Learn: ANDROID+ Ressource Qualifiers

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.hellodevs.training.helloworld
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
-//ANDROID+ Context et Application
+//ANDROID+ Ressource Qualifiers
 /*
-Classe abstraite local (ex: Activity) ou global (ex:Application)
-Si Objet attaché à la vie de l'activity utiliser context activity
-Si Objet attaché à la vie de l'application utiliser context application (ex connection a une bdd)
+Permet de définir les cas d'utilisation des resources
  */
 class MainActivity : AppCompatActivity() {
 
@@ -21,25 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i(TAG, "onCreate()")
 
-        val textViewActivity = TextView(this)
-        textViewActivity.text = "Activity Context"
-        textViewActivity.textSize = 30f
-
-        val textViewApp = TextView(applicationContext)
-        textViewApp.text = "Application Context"
-        textViewApp.textSize = 30f
-
-        val layout: LinearLayout = root
-        layout.addView(textViewActivity)
-        layout.addView(textViewApp) //Le theme ne sera pas appliqué
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(TAG, "onDestroy()")
-    }
+
 }
 
 
