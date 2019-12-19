a94d2c6ed3f83037d7931f5dde5e4b0e395c3dfe
hello-devs <cirer972@gmail.com>
2019-12-19 12:22:50 +0100

    ANDROID+ Ressource Qualifiers i18n, pluriels, strings format

--- app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
+++ app/src/main/java/com/hellodevs/training/helloworld/MainActivity.kt
@@ -2,8 +2,9 @@ package com.hellodevs.training.helloworld
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-//ANDROID+ Ressource Qualifiers
+//ANDROID+ Ressource Qualifiers i18n, pluriels, formatage de chaînes
 /*
 Permet de définir les cas d'utilisation des resources
  */
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        label_flour.text = getString(R.string.label_flour, 300)
+
+        val eggCount = 2
+        label_eggs.text = resources.getQuantityString(R.plurals.number_of_eggs, eggCount, eggCount)
 
     }
 
